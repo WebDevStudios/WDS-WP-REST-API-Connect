cd7f682acf53c0180fcb9e027c4037a0353e372b
Justin Sternberg <justin@dsgnwrks.pro>
2014-10-02 14:39:20 -0400

    Handle errors more gracefully if WP_DEBUG is off

--- WP_JSON_API_Connect.php
+++ WP_JSON_API_Connect.php
@@ -473,8 +473,26 @@ class WP_JSON_API_Connect {
 
 		$response = wp_remote_get( $this->args['json_url'] );
 		$body = wp_remote_retrieve_body( $response );
+
 		if ( ! $body ) {
-			throw new Exception( "Could not retrive body from {$this->args['json_url']}." );
+			$error_message = sprintf( __( 'Could not retrive body from URL: "%s"', 'WP_JSON_API_Connect' ), $this->args['json_url'] );
+
+			if ( defined( 'WP_DEBUG' ) ) {
+
+				error_log( 'request args: '. print_r( $this->args, true ) );
+				error_log( 'request response: '. print_r( $response, true ) );
+				throw new Exception( $error_message );
+
+			} else {
+
+				delete_option( 'wp_json_api_connect_error' );
+				add_option( 'wp_json_api_connect_error', array(
+					'message'          => $error_message,
+					'request_args'     => print_r( $this->args, true ),
+					'request_response' => print_r( $response, true ),
+				), '', 'no' );
+
+			}
 		}
 
 		$this->json_desc = $body && ( $json = $this->is_json( $body ) ) ? $json : false;
